3742801ed3989ca04bb7a21be78960804e8f8b60
goxibyte <denissmolonski@gmail.com>
2025-05-26 13:05:24 +0200

    feat(gravity-mainnet): add proxy and safe addresses

--- safe_eth/safe/addresses.py
+++ safe_eth/safe/addresses.py
@@ -2173,6 +2173,20 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
             "1.3.0+L2",
         ),  # v1.3.0+L2
     ],
+    EthereumNetwork.GRAVITY_ALPHA_MAINNET: [
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 96, "1.3.0"),  # v1.3.0
+        (
+            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
+            96,
+            "1.3.0+L2",
+        ),  # v1.3.0+L2
+        ("0x41675C099F32341bf84BFc5382aF534df5C7461a", 96, "1.4.1"),  # v1.4.1
+        (
+            "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+            96,
+            "1.4.1+L2",
+        ),  # v1.4.1+L2
+    ]
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -3224,6 +3238,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.ABSTRACT_SEPOLIA_TESTNET: [
         ("0xDAec33641865E4651fB43181C6DB6f7232Ee91c2", 2207746),  # v1.3.0
+    ],
+    EthereumNetwork.GRAVITY_ALPHA_MAINNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 88),  # v1.3.0
+        ("0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67", 88),  # v1.4.1
     ],
 }
 
